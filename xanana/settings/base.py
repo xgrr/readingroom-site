aadda7bddda31bb0b441b4514c992aac3830b3dd
Joshua Brooks <josh.vdbroek@gmail.com>
2019-08-29 09:00:02 +0900

    settings

--- xanana/settings/base.py
+++ xanana/settings/base.py
@@ -89,14 +89,6 @@ WSGI_APPLICATION = "xanana.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DATABASE_ENGINE','django.db.backends.postgresql'),
